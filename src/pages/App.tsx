a5dce1df0f8015ac959561c2be92abdaf4e06d0e
Julia Kroger <juliakroger@gmail.com>
2025-12-26 17:21:23 -0300

    init vite app

---
+++ src/pages/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div className="text-white p-4 flex flex-col gap-4">Game here</div>;
+};
+
+export default App;
---
+++ src/utils/cardDeck.ts
@@ -0,0 +1,23 @@
+export const CARD_TYPES = {
+  HEART: "HEART",
+  DIAMONDS: "DIAMONDS",
+  CLUBS: "CLUBS",
+  SPADES: "SPADES",
+};
+
+export const CARD_VALUES = {
+  A: "A",
+  1: 1,
+  2: 2,
+  3: 3,
+  4: 4,
+  5: 5,
+  6: 6,
+  7: 7,
+  8: 8,
+  9: 9,
+  10: 10,
+  J: "J",
+  Q: "Q",
+  K: "K",
+};
